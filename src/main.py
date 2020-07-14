b1261ae763402fa7f125946fb6450a9f9c628b87
Arda <ardaarslan064@gmail.com>
2020-07-14 19:48:15 +0300

    You can now choose any docx file you want from file dialog box. sys.exit added if user do not choose a docx file.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
+import sys
+
 import Exam as ex
 import xml.etree.ElementTree as ET
 from lxml import etree
 import copy
 import random
+import tkinter as tk
+from tkinter import filedialog
+
 
 if __name__ == "__main__":
 
@@ -10,7 +15,18 @@ if __name__ == "__main__":
     dict = {}
     paragraphs = []
 
-    exam = ex.Exam('inputs/input1.docx')
+
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename()
+
+    control = []
+    control = file_path.split('.')
+    if control[1] != 'docx':
+        print('You must choose a docx file to continue!')
+        sys.exit()
+
+    exam = ex.Exam(file_path)
 
     exam.produceExamGroups(5)
-    print("deneme commiti")
+
